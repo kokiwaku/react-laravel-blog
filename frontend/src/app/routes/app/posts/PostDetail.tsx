dba1b05c6c8e61e0a5a531ab7a27960de87c0d50
koki waku <kokiwaku1212@gmail.com>
2025-07-31 22:29:49 +0900

    [add] ユーザーが閲覧するコンテンツのlayoutのベースを作成

---
+++ frontend/src/app/routes/app/posts/PostDetail.tsx
@@ -0,0 +1,13 @@
+import { ContentLayout } from "@/components/layouts/ContentLayout";
+
+const PostDetail = () => {
+  return (
+    <>
+      <ContentLayout>
+        <h1>PostDetail</h1>
+      </ContentLayout>
+    </>
+  );
+};
+
+export default PostDetail;
