14b251a796bbe7cd6541cdece18d39af9b9c1c12
koki waku <kokiwaku1212@gmail.com>
2025-08-01 09:51:35 +0900

    [add] フッターを作成

--- frontend/src/components/layouts/ContentLayout.tsx
+++ frontend/src/components/layouts/ContentLayout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 export const ContentLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="container mx-auto py-8">
       <Header />
       {children}
+      <Footer />
     </div>
   );
 };
